68169a36658fd524b532f31112c92cf694e3aca8
Tom Flatters <tflats.tf@gmail.com>
2019-10-10 17:54:51 +0100

    2.29 finished

--- chapter 2/2.29.scm
+++ chapter 2/2.29.scm
@@ -33,3 +33,31 @@
         (make-branch 9 8)))))
 
 (get-weight m1) 
+
+; c
+(define (is-branch-balanced? mob)
+    (= 
+        (* (get-weight (branch-structure (left-branch mob))) (branch-length (left-branch mob)))
+        (* (get-weight (branch-structure (right-branch mob))) (branch-length (right-branch mob)))))
+
+(define (is-balanced? mob)
+    (cond 
+        ((null? mob) #t)
+        ((not(pair? mob)) #t)
+        ((is-branch-balanced? mob) (and (is-balanced? (branch-structure (left-branch mob))) (is-balanced? (branch-structure (right-branch mob)))))
+        (else #f)))
+
+(define m2 (make-mobile 
+    (make-branch 4 6) 
+    (make-branch 
+        2 
+        (make-mobile 
+            (make-branch 5 8) 
+            (make-branch 10 4))))) 
+
+(is-balanced? m1)
+(is-balanced? m2)
+
+; d
+; if we redefined the constructors, all we have to do is redefine the selectors in the same level of abstraction.
+; (so we'd just have to change left-branch, right-branch, branch-length, branch-structure)
